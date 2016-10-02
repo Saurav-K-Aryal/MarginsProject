be5e24365bd8038e7482ee77019f35bdcaf9d625
Sudheesh Bhattarai <sudheesh.bhattarai@bison.howard.edu>
2016-10-01 21:30:14 -0400

    Reading and writing happens in the same file. Added some documentations.

--- Margins.cpp
+++ Margins.cpp
@@ -1,84 +1,80 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <cstring>
 #include <cstdlib>
 #include <iomanip>
 
 
 using namespace std;
 
-const static char PAD_CHAR = ' ';
 
-// string StringPadRight(string* string, int padded_len, char *pad) {
-//     int len = string->length();
-//     if (len >= padded_len) {
-//         return *string;
-//     }
-//     int i;
-//     for (i = 0; i < padded_len - len; i++) {
-//         strcat(string.c_str(), pad);
-//     }
-//     return *string;
-// }
+//This is the paddingElement. This is used for padding. We are using '.' for padding. User have the option of adding the paramter that they want.
+const static char paddingElement = ' ';
+//The maximum for the character
+const static int maxCharacterLimit = 80;
+/**
+   This function is used for the right padding of the file.
+*/
 
-void rpad(string* buffer, int right, int left, char PAD_CHAR, ostream& out) {
-   int MAX_CHAR_LIMIT = 80; 
-   out << setiosflags(ios::left) << setw(right + MAX_CHAR_LIMIT + left) << setfill(PAD_CHAR) << *buffer << "\n";
+void rightPadding(string* buffer, int right, int left, char paddingElement, ostream& out) {
+   out << setiosflags(ios::left) << setw(right + maxCharacterLimit + left) << setfill(paddingElement) << *buffer << "\n";
 }
-//    string tmp;
-//    out.setf(ios_base::right);   // Tell the stream to
-//                                 // right-justify
-//    while (!in.eof()) {
-//       out.width(right);                  // Reset width after
-//       getline(in, tmp, '\n');        // each write
-//       out << tmp << '\n';
-//    }
-//    out.close();
-// }
 
-// addMargins takes two streams and two numbers. The streams are for
-// input and output. The first of the two numbers represents the
-// left margin width (i.e., the number of spaces to insert at the 
-// beginning of every line in the file). The second number represents
-// the total line width to pad to.
+/**
+   This function takes the file, left and right margins. 
+   This adds the left margin and calls the function above for  the right padding.
+   This function also writes to the file with the proper margins.
+*/
 void addMargins(istream& in, ostream& out, int left, int right) {
 
    string tmp;
 
    while (!in.eof()) {
-      // tmp = &temp;
       getline(in, tmp, '\n');
-      tmp.insert(tmp.begin(), left, PAD_CHAR);
-      cout << tmp;
-      rpad(&tmp, right, left, PAD_CHAR, out);                 // rpad from Recipe
-      //out << tmp << '\n';
+      tmp.insert(tmp.begin(), left, paddingElement);
+      rightPadding(&tmp, right, left, paddingElement, out);    
    }
 }
 
-int main(int argc, char** argv) {
 
+/**
+   This is the main function. It takes the file, left and right margin as the parameter.
+   It checks to see if there is a proper number of arguments in the parameter and converts the left and right margin to integer.
+   It calls the add margin function above.
+*/
+int main(int argc, char** argv) {
+   /*
+      The temp file is used as a buffer till we read from the
+      DAT1.txt file, do the padding and than write it again to
+      DAT.txt file.
+   */
+   int left, right;
+   string readingFromTempFile;
    if (argc != 4)
       return(EXIT_FAILURE);
-
    ifstream in(argv[1]);
    ofstream out("temp.txt");
-
    if (!in || !out)
-      return(EXIT_FAILURE);
-    int left, right;
+      return(EXIT_FAILURE);   
    if (argc == 4) {
       left = atoi(argv[2]);
       right = atoi(argv[3]);
       addMargins(in, out, left, right);
    } 
-
-
-
+   in.close();
    out.close();
-
+   ifstream input("temp.txt");
+   ofstream output(argv[1]);
+   //The following takes the temp file as input and writes to the DAT1.txt ile
+   for(int i=0;input.eof()!=true;i++)
+      readingFromTempFile+=input.get();
+   readingFromTempFile.erase(readingFromTempFile.end()-1);
+   output<<readingFromTempFile;
+   remove("temp.txt");//deleting the temp file
+   //Used to see if the program successfully did the work
    if (out)
-      return(EXIT_SUCCESS);
+      cout<<"\nProgram successfully completed\n";
    else
-      return(EXIT_FAILURE);
+      cout<<"\nERROR with the program\n";
+   return 0;
 }
